5623c251732db2ec6391d7d30a303185594fb9a2
ChhotuRaj07 <chhotukr825412@gmail.com>
2026-08-30 10:38:04 +0530

    Crete a login Page For User Authenticationn

---
+++ frontend/src/pages/Login.tsx
@@ -0,0 +1,21 @@
+import {useState } from "react";
+
+// I create a Login Page for User Authentication 
+export default function AuthFoam() {
+    const [isSignup, setIsSignup] = useState (false);
+    const [email , setEmail] = useState("");
+    const [password, setpassword]= useState("");
+    const [error, setError] =  useState ("");
+    const [loading , setLoading] = useState(false);
+}
+
+async function handleSubmit(e) {
+    e.preventDefault();
+    setError("");   
+}
+
+    if (!email || !password) {
+        seterr
+    }
+
+  
